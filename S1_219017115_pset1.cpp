3d372476cace917312996a3bf3d08f63c45658c1
219017115 <63197429+219017115@users.noreply.github.com>
2020-04-05 17:10:20 +0200

    Create S1_219017115_pset1.cpp

---
+++ S1_219017115_pset1.cpp
@@ -0,0 +1,11 @@
+//219017115
+#include<iostream>
+#using namespace std;
+
+int main ()
+{
+	char value;
+	cout << "enter your value" << endl;
+	cin >> value;
+	cout << "value entered" << endl;
+	return 0;
